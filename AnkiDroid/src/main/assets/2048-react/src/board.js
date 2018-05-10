417090fe614e3ab06e1fab0902c44fb6e10e7697
Sebastian <anse23@hotmail.com>
2018-05-10 20:04:27 +0100

    feat: Modified value of new tile
    Now, new tiles have value 2 always.

--- AnkiDroid/src/main/assets/2048-react/src/board.js
+++ AnkiDroid/src/main/assets/2048-react/src/board.js
@@ -131,7 +131,8 @@ Board.prototype.addRandomTile = function () {
   }
   var index = ~~(Math.random() * emptyCells.length);
   var cell = emptyCells[index];
-  var newValue = Math.random() < Board.fourProbability ? 4 : 2;
+  //var newValue = Math.random() < Board.fourProbability ? 4 : 2;
+  var newValue = 2;
   this.cells[cell.r][cell.c] = this.addTile(newValue);
 };
 
